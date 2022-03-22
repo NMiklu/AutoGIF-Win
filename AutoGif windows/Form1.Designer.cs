398c03bd033a9396f6251051d65e0c990034f689
HenryCullom <54919530+HenryCullom@users.noreply.github.com>
2022-03-22 16:13:01 -0400

    added files from original github repo

---
+++ AutoGif windows/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace AutoGif_windows
+{
+    partial class main_window
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.Location = new System.Drawing.Point(16, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(486, 137);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Recording";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // main_window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 170);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.Name = "main_window";
+            this.Text = "AutoGIF";
+            this.Load += new System.EventHandler(this.main_window_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ AutoGif windows/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GifMotion;
+using System.IO;
+using AForge.Video;
+
+namespace AutoGif_windows
+{
+    public partial class main_window : Form
+    {
+        bool recording;
+        ScreenCaptureStream scr;
+        List<Image> frame_list = new List<Image>();
+        public main_window()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (recording)
+            {
+                scr.SignalToStop();
+                scr.WaitForStop();
+                string workingPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+                using (GifCreator gifCreator = AnimatedGif.Create(workingPath + "/test.gif", 33))
+                {
+                    foreach (Image img in frame_list) 
+                    {
+                        using (img) 
+                        {
+                            // Add the image to gifEncoder with default Quality
+                            Console.WriteLine(img);
+                            Console.WriteLine("HULLO GAMERS");
+                            //gifCreator.AddFrame(img);
+                        } // Image disposed
+                    }
+                    
+                }
+                button1.Text = "Start Recording";
+                button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+                recording = false;
+            } else {
+                scr.Start();
+                button1.Text = "Stop Recording";
+                button1.BackColor = System.Drawing.Color.Red;
+                recording = true;
+            }
+            
+        }
+
+        private void main_window_Load(object sender, EventArgs e)
+        {
+            Rectangle screenArea = Rectangle.Empty;
+            foreach (System.Windows.Forms.Screen screen in System.Windows.Forms.Screen.AllScreens)
+            {
+                screenArea = Rectangle.Union(screenArea, screen.Bounds);
+            }
+            recording = false;
+            scr = new ScreenCaptureStream(screenArea);
+            scr.NewFrame += new NewFrameEventHandler(video_NewFrame);
+        }
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // get new frame
+            Bitmap bmp = eventArgs.Frame;
+            frame_list.Add(bmp);
+        }
+    }
+}
