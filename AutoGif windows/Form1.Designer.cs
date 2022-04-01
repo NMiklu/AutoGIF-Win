31b5b4207cfe889f635f51d9ed64e9300897d296
Henry Cullom <henrycullom@gmail.com>
2022-04-01 16:14:41 -0400

    tried to add a progress bar, threading issues

--- AutoGif windows/Form1.Designer.cs
+++ AutoGif windows/Form1.Designer.cs
@@ -33,13 +33,14 @@ namespace AutoGif_windows
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.button1.Location = new System.Drawing.Point(16, 17);
+            this.button1.Location = new System.Drawing.Point(16, 12);
             this.button1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(1080, 55);
@@ -50,9 +51,9 @@ namespace AutoGif_windows
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(16, 83);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 83);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1080, 757);
+            this.pictureBox1.Size = new System.Drawing.Size(1084, 757);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -61,6 +62,14 @@ namespace AutoGif_windows
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1088, 58);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
             // main_window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
@@ -68,6 +77,7 @@ namespace AutoGif_windows
             this.ClientSize = new System.Drawing.Size(1112, 852);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
             this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.Name = "main_window";
             this.Text = "AutoGIF";
@@ -81,5 +91,6 @@ namespace AutoGif_windows
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
--- AutoGif windows/Form1.cs
+++ AutoGif windows/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 using GifMotion;
 using System.IO;
@@ -24,10 +25,32 @@ namespace AutoGif_windows
         String path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
         GifCreator gifCreator;
         List<Image> frame_list;
+        Thread gifThread;
+        bool processingFramesDone = false;
         public main_window()
         {
             InitializeComponent();
         }
+
+        private void processFrames()
+        {
+            foreach (Image img in frame_list) {
+                gifCreator.AddFrame(img, delay: -1, quality: GIFQuality.Bit8);
+                progressBar1.PerformStep();
+            }
+            // when thread finishes, reshow button
+            gifCreator.Dispose();
+
+            progressBar1.Visible = false;
+            button1.Visible = true;
+            button1.BringToFront();
+
+            button1.Text = "Start Recording";
+            button1.BackColor = System.Drawing.Color.SeaGreen;
+            recording = false;
+            processingFramesDone = true;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             if (!recording)
@@ -36,21 +59,29 @@ namespace AutoGif_windows
                 gifCreator = AnimatedGif.Create(path + "/test.gif", 33);
                 timer1.Start();
 
-
                 button1.Text = "Stop Recording";
                 button1.BackColor = System.Drawing.Color.Red;
                 recording = true;
             }
             else
             {
-                timer1.Stop();
-                gifCreator.Dispose();
+                if (!processingFramesDone)
+                {
+                    // change button to show a progress bar
+                    button1.Visible = false;
+                    button1.SendToBack();
+                    progressBar1.Visible = true;
+                    progressBar1.Maximum = frame_list.Count;
+                    progressBar1.Minimum = 1;
+                    progressBar1.Value = 1;
+                    progressBar1.Step = 1;
+                    timer1.Stop();
 
-                button1.Text = "Start Recording";
-                button1.BackColor = System.Drawing.Color.SeaGreen;
-                recording = false;
+                    // create new thread to process frames so program doesn't stall
+                    gifThread = new Thread(processFrames);
+                    gifThread.Start();
+                }
             }
-
         }
 
         private void main_window_Load(object sender, EventArgs e)
@@ -58,6 +89,7 @@ namespace AutoGif_windows
             timer1 = new System.Windows.Forms.Timer();
             timer1.Interval = FPS;
             timer1.Tick += timer1_Tick;
+            progressBar1.Visible = false;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -68,7 +100,6 @@ namespace AutoGif_windows
             g.CopyFromScreen(0, 0, 0, 0, img.Size);
             pictureBox1.Image = img;
             frame_list.Add(img);
-            gifCreator.AddFrame(img, delay: -1, quality: GIFQuality.Bit8);
         }
     }
 }
