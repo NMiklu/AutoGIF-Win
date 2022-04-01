3af719c340d8680364f6e33381059dcb15291fc7
Henry Cullom <henrycullom@gmail.com>
2022-04-01 17:12:03 -0400

    redisigned interface, works, but no delay or time control yet

--- AutoGif windows/Form1.Designer.cs
+++ AutoGif windows/Form1.Designer.cs
@@ -30,20 +30,29 @@ namespace AutoGif_windows
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_window));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.delayBar = new System.Windows.Forms.TrackBar();
+            this.timerBar = new System.Windows.Forms.TrackBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.delayBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.button1.Location = new System.Drawing.Point(16, 12);
+            this.button1.Font = new System.Drawing.Font("Tahoma", 15.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(923, 12);
             this.button1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(1080, 55);
+            this.button1.Size = new System.Drawing.Size(365, 189);
             this.button1.TabIndex = 1;
             this.button1.Text = "Start Recording";
             this.button1.UseVisualStyleBackColor = false;
@@ -51,9 +60,9 @@ namespace AutoGif_windows
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 83);
+            this.pictureBox1.Location = new System.Drawing.Point(28, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1084, 757);
+            this.pictureBox1.Size = new System.Drawing.Size(854, 480);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -64,26 +73,59 @@ namespace AutoGif_windows
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Location = new System.Drawing.Point(909, 12);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(1088, 58);
+            this.progressBar1.Size = new System.Drawing.Size(395, 189);
             this.progressBar1.TabIndex = 3;
             this.progressBar1.Visible = false;
+            // 
+            // delayBar
+            // 
+            this.delayBar.Location = new System.Drawing.Point(457, 561);
+            this.delayBar.Name = "delayBar";
+            this.delayBar.Size = new System.Drawing.Size(860, 114);
+            this.delayBar.TabIndex = 4;
+            // 
+            // timerBar
+            // 
+            this.timerBar.Location = new System.Drawing.Point(457, 667);
+            this.timerBar.Name = "timerBar";
+            this.timerBar.Size = new System.Drawing.Size(860, 114);
+            this.timerBar.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(922, 212);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(365, 280);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
             // 
             // main_window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1112, 852);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(4)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(1329, 802);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.timerBar);
+            this.Controls.Add(this.delayBar);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.Name = "main_window";
             this.Text = "AutoGIF";
             this.Load += new System.EventHandler(this.main_window_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.delayBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -92,5 +134,8 @@ namespace AutoGif_windows
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TrackBar delayBar;
+        private System.Windows.Forms.TrackBar timerBar;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
